f98a49cd290e817d17043f60735117acea191c47
admiiinator <admiiinator@gmail.com>
2023-06-20 12:34:02 +0900

    Modified some files

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var fs = require('node:fs');
+const { fstat } = require('fs');
 
 var app = express();
 
@@ -19,8 +18,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+function regRoutes(p='') {
+  let r = 'routes'
+  fs.readdirSync(`./${r}/${p}`).forEach(f => {
+    fs.statSync(`./${r}/${p}/${f}`).isDirectory()
+      ? (f !== "dist" && f !== "utils") && regRoutes(`${p}/${f}`)
+      : app.use(`/${p}/${f.split('.')[0]}`, require(`./${r}/${p}/${f}`));
+  });
+}
+
+regRoutes();
+
+app.get('/', (req, res, next) => {
+  res.status(301).redirect('/home');
+})
+
+app.use('/home', require('./routes/home'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
